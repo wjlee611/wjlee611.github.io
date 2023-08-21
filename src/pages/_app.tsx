80f4ff41d810b8c54b1d01d52acc223c6d53bc5d
wjlee611 <wjlee611@gmail.com>
2023-08-21 17:39:42 +0900

    add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import "@/styles/globals.css";
+import ProgressBar from "@badrap/bar-of-progress";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { Router } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
+const progress = new ProgressBar({
+  size: 2,
+  color: "#fe9599",
+  className: "bar-of-progress",
+  delay: 80,
+});
+
+if (typeof window !== "undefined") {
+  progress.start();
+  progress.finish();
+}
+
+Router.events.on("routeChangeStart", () => progress.start());
+Router.events.on("routeChangeComplete", () => progress.finish());
+Router.events.on("routeChangeError", () => progress.finish());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
