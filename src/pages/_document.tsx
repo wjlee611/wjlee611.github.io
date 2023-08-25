f5f06c2307ec7e2656867fee5ca91217e67ab081
wjlee611 <wjlee611@gmail.com>
2023-08-25 22:33:56 +0900

    transition test 2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
+import { Router } from "next/router";
+import clsWrapper from "@/utils/class-wrapper";
+
+let onProgress: boolean = false;
 
 export default function Document() {
+  const start = () => {
+    if (onProgress) {
+      return;
+    }
+    onProgress = true;
+  };
+
+  const finish = () => {
+    onProgress = false;
+  };
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", finish);
+    Router.events.on("routeChangeError", finish);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", finish);
+      Router.events.off("routeChangeError", finish);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Router]);
+
   return (
     <Html lang="ko">
       <Head>
@@ -10,7 +38,11 @@ export default function Document() {
         <meta name="application-name" content="웅덩이" />
         <meta name="theme-color" content="#009c39" />
       </Head>
-      <body>
+      <body
+        className={clsWrapper(
+          onProgress ? "scrollbar-hide overflow-hidden" : ""
+        )}
+      >
         <Main />
         <NextScript />
       </body>
