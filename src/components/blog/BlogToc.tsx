6a0794c2d91b73e3900dc271ce25d99e70116ef5
wjlee611 <wjlee611@gmail.com>
2023-09-10 10:32:50 +0900

    add table of contents

---
+++ src/components/blog/BlogToc.tsx
@@ -0,0 +1,150 @@
+import clsWrapper from "@/utils/class-wrapper";
+import { Fragment, useEffect, useState } from "react";
+
+export type Toc = Section[];
+export type SubSection = {
+  slug: string;
+  text: string;
+};
+export type Section = SubSection & {
+  subSections: SubSection[];
+};
+
+const useScroll = (toc: Toc) => {
+  const [currentSectionSlug, setCurrentSectionSlug] = useState<
+    string | undefined
+  >();
+
+  useEffect(() => {
+    if (toc.length === 0) return;
+
+    let headings: { id: string; top: number }[];
+    const style = window.getComputedStyle(document.documentElement);
+    const scrollMt =
+      parseFloat(
+        style.getPropertyValue("--scroll-mt").match(/[\d.]+/)?.[0] ?? "0"
+      ) * parseFloat(style.fontSize.match(/[\d.]+/)?.[0] ?? "16");
+
+    function onResize() {
+      headings = Array.from(
+        document.querySelectorAll<HTMLElement>(
+          ".prose h2:not(#table-of-contents),h3:not(#table-of-contents)"
+        )
+      ).map((element) => ({ id: element.id, top: element.offsetTop }));
+    }
+
+    function onScroll() {
+      if (!headings) return;
+
+      const NAV_TOP = -120;
+      const top = window.pageYOffset + scrollMt - NAV_TOP + 1;
+
+      let current: typeof currentSectionSlug = undefined;
+      for (let i = 0; i < headings.length; i++) {
+        if (top >= headings[i].top) {
+          current = headings[i].id;
+        }
+      }
+      setCurrentSectionSlug(current);
+    }
+
+    onResize();
+    onScroll();
+    window.addEventListener("scroll", onScroll, {
+      capture: true,
+      passive: true,
+    });
+    window.addEventListener("resize", onResize, {
+      capture: true,
+      passive: true,
+    });
+    return () => {
+      window.removeEventListener("scroll", onScroll, { capture: true });
+      window.removeEventListener("resize", onResize, { capture: true });
+    };
+  }, [toc]);
+
+  return { toc, currentSectionSlug };
+};
+
+export default function BlogToc({ toc }: { toc: Toc }) {
+  const { currentSectionSlug } = useScroll(toc);
+
+  const isSubSectionActive = (subSection: SubSection) => {
+    return subSection.slug === currentSectionSlug;
+  };
+
+  const isSectionActive = (section: Section) => {
+    return (
+      section.slug === currentSectionSlug ||
+      section.subSections.some((v) => v.slug === currentSectionSlug)
+    );
+  };
+
+  return (
+    <div className="h-fit hidden lg:flex flex-col overflow-hidden rounded-xl transition-all sticky top-32 bg-blue-400">
+      {toc.length !== 0 && (
+        <div className="">
+          <p
+            id="toc-header"
+            className="py-3 font-extrabold leading-6 bg-blue-500 text-white"
+          >
+            🫧 한 눈에 보기
+          </p>
+          <ul
+            id="toc-content"
+            className="px-4 pb-2 mt-2 flex flex-col items-start justify-start text-sm text-start"
+          >
+            {toc.map((section) => (
+              <Fragment key={section.slug}>
+                <li>
+                  <a
+                    href={`#${section.slug}`}
+                    className={clsWrapper(
+                      "group block py-1",
+                      section.subSections && "",
+                      isSectionActive(section)
+                        ? "bg-gradient-to-r from-blue-900 to-blue-600 bg-clip-text font-bold text-transparent"
+                        : "text-white hover:text-blue-600"
+                    )}
+                  >
+                    {section.text}
+                  </a>
+                </li>
+                {section.subSections.map((subSection) => (
+                  <li key={subSection.slug} className="ml-4">
+                    <a
+                      href={`#${subSection.slug}`}
+                      className={clsWrapper(
+                        "group flex items-start py-1",
+                        isSubSectionActive(subSection)
+                          ? "bg-gradient-to-r from-blue-900 to-blue-600 bg-clip-text font-bold text-transparent"
+                          : "text-white hover:text-blue-600"
+                      )}
+                    >
+                      <svg
+                        width="3"
+                        height="24"
+                        viewBox="0 -9 3 24"
+                        className="mr-2 overflow-visible text-white"
+                      >
+                        <path
+                          d="M0 0L3 3L0 6"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                        />
+                      </svg>
+                      {subSection.text}
+                    </a>
+                  </li>
+                ))}
+              </Fragment>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/blog/[...slugs].tsx
+++ src/pages/blog/[...slugs].tsx
@@ -1,8 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import { Post, getAllPosts } from "../../libs/posts";
-import { serializeMdx } from "@/libs/mdx";
+import { parseToc, serializeMdx } from "@/libs/mdx";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import Layout from "@/components/common/Layout";
+import BlogToc, { Section } from "@/components/blog/BlogToc";
 
 export const getStaticPaths: GetStaticPaths = () => {
   const posts = getAllPosts();
@@ -31,6 +32,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
       mdx,
+      toc: parseToc(post.content),
     },
   };
 };
@@ -38,30 +40,38 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 interface IPostPage {
   post: Post;
   mdx: MDXRemoteSerializeResult;
+  toc: Section[];
 }
 
-export default function PostPage({ post, mdx }: IPostPage) {
+export default function PostPage({ post, mdx, toc }: IPostPage) {
   return (
     <Layout>
-      <div className="prose dark:prose-dark flex flex-col items-start pb-16">
-        <header className="flex w-full flex-col items-center mb-10">
-          <h1 className="text-blue-500">{post.title}</h1>
+      <div className="flex flex-col items-start pb-16">
+        <header className="flex w-full flex-col items-center my-10">
+          <h1 className="text-blue-500 text-3xl font-bold mb-5">
+            {post.title}
+          </h1>
           <span className="text-blue-500 text-lg">{post.description}</span>
-          <span className="space-x-10 mt-7 text-blue-400">
+          <span className="space-x-10 mt-5 text-blue-400 text-sm">
             읽는데
-            <span className="text-blue-500 text-2xl">
+            <span className="text-blue-500 text-xl">
               {" "}
               {post.readingMinutes}
             </span>
             분 정도 걸려요.
           </span>
         </header>
-        <MDXRemote {...mdx} />
+        <section className="w-full flex justify-between space-x-5">
+          <div className="w-full flex flex-col prose dark:prose-dark max-w-full lg:max-w-2xl 2xl:max-w-4xl">
+            <MDXRemote {...mdx} />
+          </div>
+          <BlogToc toc={toc} />
+        </section>
         <footer className="flex flex-col mt-10">
-          <span className="text-blue-400 italic">
+          <span className="text-blue-400 italic text-start">
             {post.date} 에 작성되었어요.
           </span>
-          <div className="flex items-center">
+          <div className="flex items-center mt-5 space-x-5">
             <span>Tag</span>
             <ul className="flex space-x-3 list-none m-0">
               {post.tags.map((tag, i) => (
